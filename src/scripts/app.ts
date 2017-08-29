6fc3e083e4b5fddc410103c8b9d0734b49b29fdc
SoapKim <z657107606@163.com>
2017-08-29 17:30:27 +0800

    update d3 demo

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,38 +1,11 @@
 import "../styles/base.scss";
-
-
-// 引入three.js
-import * as THREE from "three";
-
-// OrbitControls.js expects a global THREE object
-(window as any).THREE = THREE;
-
-// NOTE: OrbitControls must be included with require:
-// using "import" cause it to be executed before global THREE becomes available
-import "three/examples/js/controls/OrbitControls";
-//require("three/examples/js/controls/OrbitControls");
-
-//THREE.OrbitControls = require('imports?THREE=three!exports?THREE.OrbitControls!../../node_modules\/three\/examples\/js\/controls\/OrbitControls');
-
-// ... code that uses THREE and THREE.OrbitControls
-
-
+// import "d3/d3.min.js"
+// import * as D3 from "d3";
 // 上面引入样式资源文件
 // 业务逻辑代码从这里开始写
 
-import { Greeter } from "./greeter";
+import { GreeterD3 } from "./greeter";
 
+const greeter: GreeterD3 = new GreeterD3("this istypescritpt test demo");
 
-// OrbitControls.js expects a global THREE object
-(window as any).THREE = THREE;
-
-const greeter: Greeter = new Greeter("this istypescritpt test demo");
-
-greeter.threeStart();
-greeter.animate();
-
-
-
-//document.getElementById("greeting").innerHTML = greeter.greet();
-
-//console.log(THREE);
+greeter.init();
